ca6ef6be1681db60c374a1aff881b9eff51a4ac6
AlexCheung(张文亮) <qwe17911@sina.com>
2016-08-23 13:42:43 +0800

    仿支付宝咻一咻

---
+++ app/src/main/java/com/alex/ripplewaveview/App.java
@@ -0,0 +1,20 @@
+package com.alex.ripplewaveview;
+
+import github.alex.AlexApp;
+
+/**
+ * 作者：Alex
+ * <br/>
+ * 时间：2016/8/23 10:53
+ * <br/>
+ * 博客地址：http://www.jianshu.com/users/c3c4ea133871/subscriptions
+ * <br/>j
+ * <br/>
+ */
+public class App extends AlexApp{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
